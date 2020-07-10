eac64dbfa121d275a26c9330d6beb644454337d0
MauriceR71 <tianyulu710@gmail.com>
2020-07-09 20:44:15 -0400

    add fasta for petase language for embeddings

---
+++ get_embedding.py
@@ -0,0 +1,14 @@
+import torch
+from tape import ProteinBertModel, TAPETokenizer
+model = ProteinBertModel.from_pretrained('bert-base')
+tokenizer = TAPETokenizer(vocab='iupac')  # iupac is the vocab for TAPE models, use unirep for the UniRep model
+
+# Pfam Family: Hexapep, Clan: CL0536
+sequence = 'GCTVEDRCLIGMGAILLNGCVIGSGSLVAAGALITQ'
+token_ids = torch.tensor([tokenizer.encode(sequence)])
+output = model(token_ids)
+sequence_output = output[0]
+pooled_output = output[1]
+
+print(sequence_output.shape)
+print(pooled_output.shape)
